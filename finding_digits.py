714ca5e619d01d6b65072f09978798be91e19b87
Andrei <marinin1337@gmail.com>
2023-05-22 02:24:03 +0400

    Menu created

--- finding_digits.py
+++ finding_digits.py
@@ -20,7 +20,7 @@ def check_hash(settings: dict, first_digits: int, other_digits: int) -> int:
 def find_number(settings: dict, streams: int) -> None:
     completion = False
     with mp.Pool(streams) as pl:
-        logging.info(f'Starting card number selection: {settings["first_digits"]}******{dict["last_digits"]}')
+        logging.info(f'Starting card number selection: {settings["first_digits"]}******{settings["last_digits"]}')
         for number in pl.map(partial(check_hash, settings, int(settings["first_digits"])), tqdm(range(100000, 1000000),
                                                                                                 colour="#7e42f5")):
             if number:
@@ -28,7 +28,7 @@ def find_number(settings: dict, streams: int) -> None:
                 completion = True
                 data = {}
                 data["card_number"] = f'{number}'
-                data["Validation_check"] = luhn_algo(number)
+                data["Validation_check"] = "Unknown"
                 logging.info(f'Card number found! Saving into {settings["save_path"]}')
                 try:
                     with open(settings["save_path"], "w") as f:
@@ -42,34 +42,61 @@ def find_number(settings: dict, streams: int) -> None:
         logging.info("Card nuber not found")
 
 
-def luhn_algo(card: int) -> str:
-    card_number = f'{card}'
-    length = len(card_number)
+def luhn_algo(settings: dict) -> bool:
+    try:
+        with open(settings["save_path"], "r") as f:
+            data = json.load(f)
+    except OSError as err:
+        logging.warning(f'{err} during reading from {settings["save_path"]}')
+    number = data["card_number"]
+    length = len(number)
     if length != 16:
         logging.info("Invalid card number")
-        return "Invalid"
+        if data["Validation_check"] != "Unknown" or data["Validation_check"] != "Invalid":
+            data["Validation_check"] = "Invalid"
+            try:
+                with open(settings["save_path"], "w") as f:
+                    json.dump(data, f)
+            except OSError as err:
+                logging.warning(f'{err} during writing into {settings["save_path"]}')
+        return False
     else:
         s = 0
         for i in range(0, length - 1):
             if(length - i) % 2 == 0:
-                if (int(card_number[i]) * 2) // 10 != 0:
-                    s = s + (int(card_number[i]) * 2) // 10 + (int(card_number[i]) * 2) % 10
+                if (int(number[i]) * 2) // 10 != 0:
+                    s = s + (int(number[i]) * 2) // 10 + (int(number[i]) * 2) % 10
                 else:
-                    s += (int(card_number[i]) * 2) // 10
+                    s += (int(number[i]) * 2) // 10
             else:
-                s += int(card_number[i])
+                s += int(number[i])
         s %= 10
         s %= 10
         s = 10 - s
-        if s == card % 10:
+        if s == int(number[15]):
             logging.info("Card number is valid")
-            return "Valid"
+            if data["Validation_check"] != "Unknown" or data["Validation_check"] != "Valid":
+                data["Validation_check"] = "Valid"
+                try:
+                    with open(settings["save_path"], "w") as f:
+                        json.dump(data, f)
+                except OSError as err:
+                    logging.warning(f'{err} during writing into {settings["save_path"]}')
+            return True
         else:
             logging.info("Invalid card number")
-            return "Invalid"
+            if data["Validation_check"] != "Unknown" or data["Validation_check"] != "Invalid":
+                data["Validation_check"] = "Invalid"
+                try:
+                    with open(settings["save_path"], "w") as f:
+                        json.dump(data, f)
+                except OSError as err:
+                    logging.warning(f'{err} during writing into {settings["save_path"]}')
+            return False
 
 
 def make_statistic(settings: dict):
+    logging.info("Measuring statistic")
     times = []
     for i in range(int(settings["thread_number"])):
         start = time()
@@ -83,8 +110,5 @@ def make_statistic(settings: dict):
     plt.plot(list(x + 1 for x in range(int(settings["thread_number"]))), times, color='#004158')
     plt.savefig(f'{settings["pic_path"]}')
     logging.info(f'Picture has been saved into {settings["pic_path"]}')
-if __name__ == '__main__':
-    with open("data/settings.json", "r") as f:
-        settings = json.load(f)
-    make_statistic(settings)
+
 
